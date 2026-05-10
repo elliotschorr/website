232c7d687939f6d938b8b77247594a0851b503b5
elliotschorr <eschorr0802@gmail.com>
2026-05-10 18:25:36 -0400

    Add resume styling

--- resume/resume.js
+++ resume/resume.js
@@ -6,6 +6,11 @@ fetch("resume.json")
         resume.innerHTML = `
         <h1>${data.basics.name}</h1>
         <section class="basics">
+        <div class="resume-logos">
+                       <div class="github-icon"><a href="https://github.com/elliotschorr"><img src="/images/github-icon.png" style="max-width:25px; max-height: 25px;"></img></a></div>
+                       <div class="linkedin-icon"><a href="https://www.linkedin.com/in/elliot-schorr-040139281/"><img src="../images/linkedin-icon.png" style="max-width:25px; max-height: 25px;"></img></a></div>                    
+                       <div class="muckrack-icon"><a href="https://muckrack.com/elliot-schorr/articles"><img src="../images/muckrack-icon.png" style="max-width:30px; max-height: 30px;"></img></a></div>
+                    </div>
             <p><strong>${data.basics.label}</strong> | ${data.basics.location.city}, ${data.basics.location.region}</p>
             <p><strong>Contact:</strong> <a href="mailto:${data.basics.email}" style="color: #280D0D; text-decoration: underline;">${data.basics.email}</a></p>
             <p>${data.basics.summary}</p>
@@ -16,7 +21,7 @@ fetch("resume.json")
         ${data.work.map(job => `
             <div class="job">
                 <h3>${job.position}</h3>
-                <p><strong>${job.name}</strong> | ${job.location} | ${job.startDate} - ${job.endDate}</p>
+                <p><strong><a href="${job.url}" style="color: #280D0D; text-decoration: underline;">${job.name}</a></strong> | ${job.location} | ${job.startDate} - ${job.endDate}</p>
                 <p>${job.summary}</p>
             </div>
         `).join("")}
@@ -28,8 +33,12 @@ fetch("resume.json")
             ${data.education.map(school => `
                 <div class="school">
                     <h3>${school.studyType}'s of ${school.area}</h3>
-                    <p><strong>${school.institution}</strong> | ${school.location} | ${school.startDate} - ${school.endDate}</p>
-
+                    <p><strong><a href="${school.url}" style="color: #280D0D; text-decoration: underline;">${school.institution}</a></strong> | ${school.location} | ${school.startDate} - ${school.endDate}</p>
+                     <p>${school.summary}</p>
+                    <h3>Certifications</h3>
+                    ${data.certificates.map(certificates => `
+                      <p>${certificates.name} | <a href="${certificates.url}" style="color: #280D0D; text-decoration: underline;">${certificates.issuer}</a> | Issued ${certificates.date}
+                    `).join("")}
                 </div>
             `)}
         </section>
@@ -38,16 +47,13 @@ fetch("resume.json")
             <div class="skills-table">
                 ${data.skills.map(skill => ` 
                     <div class="skills-card">
-                        <h3>${skill.name}</h3>
-                        <p>${skill.keywords.join(", ")}</p>
+                        <a href="../work/#${skill.name.replace(/\s+/g, '-')}" style="color: inherit; text-decoration: none;">
+                            <h3>${skill.name}</h3>
+                            <p>${skill.keywords.join(", ")}</p>
+                        </a>
                     </div>
                 `).join("")}
             </div>
-        </section>
-
-        <h2>Certifications</h2>
-        <section class ="certifications">
-    
-        </section>
+        </section>      
         `;
     })
